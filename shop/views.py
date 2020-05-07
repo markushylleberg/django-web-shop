3174c8920f6b5a135cb948cf00288bf360c1890e
Markus Hylleberg <markushylleberg@gmail.com>
2020-05-07 17:42:47 +0200

    implemented search function

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.utils.crypto import get_random_string
 from django.shortcuts import render
-from .models import Product, ProductVariant, ProductAttribute
+from .models import Product, ProductVariant, ProductAttribute, ProductAttributeOption
+from django.db.models import Q
 
 def index(req):
 
@@ -45,6 +46,64 @@ def product_detail(req):
 
 def search(req):
 
-    context = {}
+    attributes_values_used = ProductAttribute.objects.values('attribute', 'value').distinct()
+
+    attributes_used = ProductAttribute.objects.values_list('attribute', flat=True).distinct()
+    attributes = ProductAttributeOption.objects.all().filter(id__in=attributes_used)
+
+    context = {
+        'values': attributes_values_used,
+        'attributes': attributes
+    }
+
+    if req.method == 'POST':
+        query_values = []
+
+        for key, value in req.POST.items():
+            if key == 'csrfmiddlewaretoken':
+                print('ignored csrf token')
+            else:
+                query_values.append(f'{value}')
+
+
+        items_found = []
+
+        for query_value in query_values:
+            if query_value:
+                items = ProductAttribute.objects.values('entity').filter(Q(value=query_value))
+                items_found.append(items)
+                # for item in items:
+                #     items_found.append(item)
+
+        filter_count = len(items_found)
+
+        print(filter_count)
+        # print(items_found)
+
+        result_list = []
+
+        final_return = []
+
+        for item_found in items_found:
+            # print(item_found)
+            for item in item_found:
+                # print(item['entity'])
+                result_list.append(item['entity'])
+
+        for item in result_list:
+            if result_list.count(item) == filter_count:
+                final_return.append(item)
+
+        final_return_query = ProductVariant.objects.all().filter(id__in=final_return)
+
+        context = {
+            'values': attributes_values_used,
+            'attributes': attributes,
+            'products': final_return_query
+        }
+
+        print(final_return_query)
+
+
 
     return render(req, 'pages/products/search.html', context)
